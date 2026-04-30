12fc1dddf093fddd515837bd4b3eda9e9a1f5d1c
Cs Technologies-30 <azharbinsethi@gmail.com>
2026-04-30 02:55:42 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/auth/Seller_Dashboard_Home/dashboard.dart';
+
+void main() {
+  runApp(FoodApp());
+}
+
+class FoodApp extends StatelessWidget {
+  const FoodApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SellerDashboard());
+  }
+}
