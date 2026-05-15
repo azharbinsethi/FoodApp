6678adcd021ac9378a1c7e927151bf4907b5b93d
Cs Technologies-30 <azharbinsethi@gmail.com>
2026-05-14 23:54:40 -0700

    API

--- lib/auth/Api/ApiExample.dart
+++ lib/auth/Api/ApiExample.dart
@@ -12,16 +12,22 @@ class ApiExample extends StatefulWidget {
 }
 
 class _ApiExampleState extends State<ApiExample> {
+  TextEditingController controller1 = TextEditingController();
   // ignore: strict_top_level_inference
+  List<Animal> animalList = [];
+  bool isLoading = false;
   var formatedData;
-  getQuotes() async {
-    var url = Uri.parse('https://api.api-ninjas.com/v2/randomquotes');
+  Animal animal = Animal();
+  getAnimals() async {
+    var url = Uri.parse(
+      'https://api.api-ninjas.com/v1/animals?name=${controller1.text}',
+    );
     final headers = {'X-Api-Key': 'cPn3DLgdAW64j3RvRWvg3QUbRpA90cK91wHJGvkd'};
 
     var response = await http.get(url, headers: headers);
-    formatedData = jsonDecode(response.body);
+    animal = jsonDecode(response.body);
     debugPrint(response.body);
-    // debugPrint(formatedData);
+    debugPrint(animal.characteristics!.averageLitterSize.toString());
     setState(() {});
   }
 
@@ -30,30 +36,130 @@ class _ApiExampleState extends State<ApiExample> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("Qoutes");
-          getQuotes();
+          print("Animals");
+          getAnimals();
           setState(() {});
         },
-        child: Text("Qoutes"),
+        child: Text("Animanls"),
       ),
-      body: formatedData == null
-          ? Center(child: CircularProgressIndicator())
-          : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    formatedData[0]['quote'],
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    formatedData[0]['author'],
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: controller1,
+              decoration: InputDecoration(border: OutlineInputBorder()),
             ),
+            ElevatedButton(
+              onPressed: () {
+                getAnimals();
+              },
+              child: Text("Search"),
+            ),
+            formatedData == null || formatedData.isEmpty
+                ? Center(child: Text("................."))
+                : Text(
+                    formatedData[0]['name'],
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+            formatedData == null || formatedData.isEmpty
+                ? Center(child: Text("................."))
+                : Text(
+                    "Taxonomy: ${formatedData[0]['taxonomy']['scientific_name']}",
+                    style: const TextStyle(fontStyle: FontStyle.italic),
+                  ),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class Animal {
+  String? name;
+  Taxonomy? taxonomy;
+  List<String>? locations;
+  Characteristics? characteristics;
+
+  Animal({this.name, this.taxonomy, this.locations, this.characteristics});
+
+  Animal.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    taxonomy = json['taxonomy'] != null
+        ? new Taxonomy.fromJson(json['taxonomy'])
+        : null;
+    locations = json['locations'].cast<String>();
+    characteristics = json['characteristics'] != null
+        ? new Characteristics.fromJson(json['characteristics'])
+        : null;
+  }
+}
+
+class Taxonomy {
+  final String scientificName;
+
+  Taxonomy({this.scientificName = "Unknown"});
+
+  factory Taxonomy.fromJson(Map<String, dynamic> json) {
+    return Taxonomy(scientificName: json['scientific_name'] ?? "Unknown");
+  }
+}
+
+class Characteristics {
+  String? temperament;
+  String? training;
+  String? diet;
+  String? averageLitterSize;
+  String? commonName;
+  String? slogan;
+  String? group;
+  String? color;
+  String? skinType;
+  String? lifespan;
+  String? height;
+
+  Characteristics({
+    this.temperament,
+    this.training,
+    this.diet,
+    this.averageLitterSize,
+    this.commonName,
+    this.slogan,
+    this.group,
+    this.color,
+    this.skinType,
+    this.lifespan,
+    this.height,
+  });
+
+  Characteristics.fromJson(Map<String, dynamic> json) {
+    temperament = json['temperament'];
+    training = json['training'];
+    diet = json['diet'];
+    averageLitterSize = json['average_litter_size'];
+    commonName = json['common_name'];
+    slogan = json['slogan'];
+    group = json['group'];
+    color = json['color'];
+    skinType = json['skin_type'];
+    lifespan = json['lifespan'];
+    height = json['height'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['temperament'] = this.temperament;
+    data['training'] = this.training;
+    data['diet'] = this.diet;
+    data['average_litter_size'] = this.averageLitterSize;
+    data['common_name'] = this.commonName;
+    data['slogan'] = this.slogan;
+    data['group'] = this.group;
+    data['color'] = this.color;
+    data['skin_type'] = this.skinType;
+    data['lifespan'] = this.lifespan;
+    data['height'] = this.height;
+    return data;
   }
 }
--- lib/auth/Home/Home_Screen.dart
+++ lib/auth/Home/Home_Screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/auth/login/authentication/login.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
@@ -12,27 +14,137 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final Titlecontroller = TextEditingController();
   final Descriptioncontroller = TextEditingController();
+  final String userId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(title: Text('Home Screen')),
-      body: ListView.builder(
-        itemCount: 15,
-
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            width: double.infinity,
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              borderRadius: BorderRadius.circular(10),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                FirebaseAuth.instance.signOut().then(
+                  (value) => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                  ),
+                );
+              },
             ),
-            child: ListTile(title: Text('hi'), subtitle: Text('bye')),
+          ],
+        ),
+      ),
+      appBar: AppBar(title: Text('Home Screen')),
+      body: StreamBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('No Data Found'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ListTile(
+                  title: Text(snapshot.data!.docs[index]['title']),
+                  subtitle: Text(snapshot.data!.docs[index]['description']),
+                  trailing: PopupMenuButton(
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        child: Text('Delete'),
+                        value: 'Delete',
+                        onTap: () {
+                          FirebaseFirestore.instance
+                              .collection('tasks')
+                              .doc(snapshot.data!.docs[index].id)
+                              .delete();
+                        },
+                      ),
+                      PopupMenuItem(
+                        child: Text('Edit'),
+                        value: 'Edit',
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text('Edit'),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextField(
+                                    controller: Titlecontroller,
+                                    decoration: InputDecoration(
+                                      hintText: 'Enter Task Name',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide(
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  TextField(
+                                    controller: Descriptioncontroller,
+                                    decoration: InputDecoration(
+                                      hintText: 'Enter Task Description',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide(
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      FirebaseFirestore.instance
+                                          .collection('tasks')
+                                          .doc(snapshot.data!.docs[index].id)
+                                          .update({
+                                            'title': Titlecontroller.text,
+                                            'description':
+                                                Descriptioncontroller.text,
+                                          });
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Update'),
+                                  ),
+                                ],
+                              ),
+                              actions: [
+                                      
+                                    ]
+                                    ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           );
         },
+        stream: FirebaseFirestore.instance
+            .collection('tasks')
+            .where('userId', isEqualTo: userId)
+            .snapshots(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -44,6 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextField(
+                    controller: Titlecontroller,
                     decoration: InputDecoration(
                       hintText: 'Enter Task Name',
                       border: OutlineInputBorder(
@@ -54,6 +167,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   SizedBox(height: 10),
                   TextField(
+                    controller: Descriptioncontroller,
+
                     decoration: InputDecoration(
                       hintText: 'Enter Task Description',
                       border: OutlineInputBorder(
@@ -79,6 +194,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         .set({
                           'title': Titlecontroller.text,
                           'description': Descriptioncontroller.text,
+                          'userId': userId,
                         })
                         .then((value) => Navigator.pop(context));
                     Navigator.pop(context);
